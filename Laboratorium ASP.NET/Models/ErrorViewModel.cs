d2537cf4ea66d42dc8d927c4a54098b4f5cdabdf
Bryczek19 <Konrad.bryk@microsoft.wsei.edu.pl>
2024-11-03 21:08:05 +0100

    lab123

---
+++ Laboratorium ASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Laboratorium_ASP.NET.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
